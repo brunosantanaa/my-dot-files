0f8dae97b9e55490373ffbb0f11060bf68c66cc0
Bruno de Santana <brunosantanaa@pop-os.localdomain>
2022-12-04 15:33:30 -0500

    fix: wqupdate

--- update.sh
+++ update.sh
@@ -1,6 +1,13 @@
 #!/bin/zsh
 
 BSA_HOME="$HOME/.bsa"
-cd $HOME && rm -rf $BSA_HOME
+# Backup
+cd $HOME && mv $BSA_HOME $BSA_HOME.backup
 
+# Install
 curl -fsSL https://raw.githubusercontent.com/brunosantanaa/my-dot-files/main/install.sh
+
+# Test
+
+# Clean
+rm -rf $BSA_HOME.backup
