f3749bee2382359ddb7a59cdd2c511fc27eb2fe6
Bruno de Santana <brunosantanaa@pop-os.localdomain>
2022-12-04 17:01:48 -0500

    feat: Git conf user

---
+++ git/config_user.zsh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+
+GIT_USER=$HOME/.gitconfig.user
+
+echo 'Config your GitHub Global user'
+echo '------------------------------'
+if [ ! -e $GIT_USER ]; then
+    echo 'Enter your name: '
+    read NAME
+    echo 'Enter your e-mail: '
+    read EMAIL
+    echo "[user]" > $GIT_USER
+	echo "\tname = ${NAME}" >> $GIT_USER
+	echo "\tmail = ${EMAIL}" >> $GIT_USER
+else
+    echo 'Current settings: '
+    cat $HOME/.gitconfig.user
+    echo 'To update your settings you can run the following command: '
+    echo 'rm ~/.gitconfig.user && ~/.bsa/git/./config_user.zsh '
+fi
