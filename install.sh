8fa313f927bedd981a4843457b1ea0ddcb84d555
Bruno de Santana <brunosantanaa@pop-os.localdomain>
2022-12-04 03:38:40 -0500

    fix: clang

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ if [ -d $BSA_DIR ]; then
 	echo "BSA dotFiles is installed"
 else
 	# Install dependencies
-	sudo apt install git neovim tmux clangd-14 clang-format-14 -y
+	sudo apt install git neovim tmux
 
     # Symbolic links
     ln -s /bin/clangd-14 /usr/bin/clangd
